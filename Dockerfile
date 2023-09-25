e6f5660739b7e2937f5109906cc176d7f4a7b42a
danialkarim807 <danialkarim8@gmail.com>
2023-09-25 17:06:46 +0500

    commit in dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
 FROM tomcat:8.0
 ADD **/*.war /opt/tomcat/for-tomcat-8.5/
-# EXPOSE 8080
-CMD ["catalina.sh", "run"]
+EXPOSE 8080
+CMD ["catalina.sh", "run", "0.0.0.0:8000"]
---
+++ lambda.py
@@ -0,0 +1,101 @@
+import boto3
+import os
+
+# Initialize AWS services
+s3 = boto3.client('s3')
+ses = boto3.client('ses')
+
+def lambda_handler(event, context):
+    # Your Lambda function logic here
+
+    # Example: Download a file from S3
+    s3_bucket = 'your-s3-bucket'
+    s3_key = 'your-file-key'
+    download_path = '/tmp/downloaded_file.txt'
+    s3.download_file(s3_bucket, s3_key, download_path)
+
+    # Example: Perform transformations
+    with open(download_path, 'r') as file:
+        data = file.read()
+        # Perform transformations here
+
+    # Example: Send an email through SES
+    sender_email = 'your-sender@example.com'
+    recipient_email = 'your-recipient@example.com'
+    subject = 'Your Subject'
+    body = 'Your Email Body'
+
+    ses.send_email(
+        Source=sender_email,
+        Destination={'ToAddresses': [recipient_email]},
+        Message={'Subject': {'Data': subject}, 'Body': {'Text': {'Data': body}}},
+    )
+
+    # Clean up temporary files
+    os.remove(download_path)
+
+    return {
+        'statusCode': 200,
+        'body': 'Lambda function executed successfully'
+    }
+# ........................................................................................
+
+
+import boto3
+
+def create_lambda_function(function_name, runtime, handler, code, environment_variables):
+    # Create a Lambda client
+    lambda_client = boto3.client('lambda')
+
+    # Define the function parameters
+    function_params = {
+        'FunctionName': function_name,
+        'Runtime': runtime,
+        'Role': 'YOUR_EXECUTION_ROLE_ARN',  # Replace with your execution role ARN
+        'Handler': handler,
+        'Code': {'ZipFile': code},
+        'Environment': {'Variables': environment_variables}
+    }
+
+    # Create the Lambda function
+    response = lambda_client.create_function(**function_params)
+
+    return response
+
+# Example usage:
+function_name = 'MyLambdaFunction'
+runtime = 'python3.8'
+handler = 'lambda_handler'
+code = '''
+def lambda_handler(event, context):
+    # Your Lambda function code here
+    pass
+'''
+environment_variables = {
+    'API_KEY': 'your_api_key',
+    'CONFIG_SETTING': 'your_config_setting'
+}
+
+create_lambda_function(function_name, runtime, handler, code, environment_variables)
+
+# --------------------------------------------------------------------------------------------------
+
+# list down all the services
+import boto3
+
+def list_aws_services(event, context):
+    # Initialize a list to store service names
+    service_names = []
+
+    # Create a session using the Lambda execution role credentials
+    session = boto3.Session()
+
+    # Iterate through available services and append their names to the list
+    for service in session.get_available_services():
+        service_names.append(service)
+
+    # Return the list of service names
+    return {
+        'statusCode': 200,
+        'body': ', '.join(service_names)
+    }
